b616f89af04980ed6467dd65990d6272c9a6f498
Young <wyoung@proforma.com>
2018-03-27 19:02:55 -0400

    Top Artists

--- public/main.js
+++ public/main.js
@@ -1,5 +1,30 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const API_KEY = "10023ddad32c6e13fbafbfacd77ae753"
+const BASE_URL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + API_KEY + "&format=json";
 
-document.addEventListener('DOMContentLoaded', main)
+angular
+  .module("mainApp", [])
+  .controller("mainController", [`$scope`, `$http`, ($scope, $http) => {
+    $scope.currentArtist =
+
+      $http({
+        method: "GET",
+        url: BASE_URL
+      }).then(response => {
+        console.log(response);
+        console.log(response.data);
+        $scope.artists = response.data.artists.artist;
+      });
+
+    $scope.loadArtists = (url) => {
+      console.log("loading artists", url)
+      $http({
+        method: "GET",
+        url: url
+      }).then(response => {
+        console.log("loading artists", response)
+        console.log(response.data);
+        $scope.artists = response.data.artists.artist;
+      });
+    }
+
+  }]);
